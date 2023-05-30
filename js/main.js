938a74c31ccc4746b098e172ebe79b7b5d645933
Tam Nguyen <tamntrung@gmail.com>
2023-05-30 16:49:26 +0000

    Added event listener to form element. #1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-const inputImage = document.querySelector('.input-image');
-const entryImage = document.querySelector('.entry-image');
+const $inputImage = document.querySelector('.input-image');
+const $entryImage = document.querySelector('.entry-image');
 
-inputImage.addEventListener('input', function (event) {
-  entryImage.setAttribute('src', inputImage.value);
+$inputImage.addEventListener('input', function (event) {
+  $entryImage.setAttribute('src', $inputImage.value);
+});
+
+const $form = document.querySelector('#new-entry');
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const newEntry = {};
+  newEntry.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.push(newEntry);
+  $entryImage.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 });
