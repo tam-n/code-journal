31fc73bb6edf4e062253674b7d562f4a4981b490
Tam Nguyen <tamntrung@gmail.com>
2023-05-30 23:17:42 +0000

    Added object properties. #1

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 const $inputImage = document.querySelector('.input-image');
 const $entryImage = document.querySelector('.entry-image');
+const $inputTitle = document.querySelector('.title');
+const $inputNotes = document.querySelector('.notes');
+const $form = document.querySelector('#new-entry');
 
 $inputImage.addEventListener('input', function (event) {
   $entryImage.setAttribute('src', $inputImage.value);
 });
 
-const $form = document.querySelector('#new-entry');
-
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
   const newEntry = {
-    title: null,
-    photoURL: null,
-    notes: null,
-    entryId: null
+    title: $inputTitle.value,
+    photoURL: $inputImage.value,
+    notes: $inputNotes.value,
+    entryId: data.nextEntryId
   };
-  newEntry.entryId = data.nextEntryId;
   data.nextEntryId++;
   data.entries.unshift(newEntry);
   $entryImage.setAttribute('src', 'images/placeholder-image-square.jpg');
