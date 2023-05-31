ac5d15dd78dd742051139b46117de5b31307b609
Tam Nguyen <tamntrung@gmail.com>
2023-05-31 18:29:48 +0000

    Defined renderEntry function. #2

--- js/main.js
+++ js/main.js
@@ -21,3 +21,35 @@ $form.addEventListener('submit', function (event) {
   $entryImage.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 });
+
+function renderEntry(entry) {
+
+  const $list = document.createElement('ul');
+  const $row = document.createElement('li');
+  const $columnPhoto = document.createElement('div');
+  const $columnNotes = document.createElement('div');
+  const $image = document.createElement('img');
+  const $header = document.createElement('h2');
+  const $notes = document.createElement('div');
+
+  $row.className = 'row';
+  $columnPhoto.className = 'column-full column-half';
+  $image.className = 'photo-url';
+  $image.setAttribute('alt', 'entry image');
+  $columnNotes.className = 'column-full column-half';
+  $header.className = 'title';
+  $notes.className = 'notes';
+
+  $list.appendChild($row);
+  $row.appendChild($columnPhoto);
+  $columnPhoto.appendChild($image);
+  $row.appendChild($columnNotes);
+
+  $image.setAttribute('src', entry.photoURL);
+  $header.textContent = entry.title;
+  $notes.textContent = entry.notes;
+
+  return $list;
+}
+
+renderEntry();
