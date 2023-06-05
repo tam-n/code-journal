1e02a923bb586461152a9ee3e8411eaf924c81fa
Tam Nguyen <tamntrung@gmail.com>
2023-06-05 02:43:55 +0000

    Created delete button and confirmation modal #4

--- js/main.js
+++ js/main.js
@@ -97,7 +97,6 @@ function toggleNoEntries() {
     $noEntry.className = 'no-entries';
   } else {
     $noEntry.className = 'no-entries-hidden';
-
   }
 }
 
@@ -110,6 +109,7 @@ function viewSwap(view) {
     $entriesView.classList.remove('hidden');
     $entryFormView.classList.add('hidden');
     $entryImage.setAttribute('src', 'images/placeholder-image-square.jpg');
+    $delete.className = 'delete-hidden';
     $form.reset();
     data.view = 'entries';
   } else if (view === 'entry-form') {
@@ -136,10 +136,12 @@ $subHeader.addEventListener('click', function (event) {
 });
 
 const $newFormTitle = document.querySelector('.new-form-title');
+const $delete = document.querySelector('.delete-hidden');
 
 $list.addEventListener('click', function (event) {
   if (event.target.matches('.fa-solid')) {
     viewSwap('entry-form');
+    $delete.className = 'delete';
     const stringNum = event.target.closest('.row').getAttribute('data-entry-id');
     const intNum = parseInt(stringNum);
     for (let i = 0; i < data.entries.length; i++) {
@@ -159,3 +161,11 @@ $list.addEventListener('click', function (event) {
   }
 }
 );
+
+const $modal = document.querySelector('.modal');
+const $overlay = document.querySelector('.overlay');
+
+$delete.addEventListener('click', function (event) {
+  $modal.classList.remove('hidden');
+  $overlay.classList.remove('hidden');
+});
